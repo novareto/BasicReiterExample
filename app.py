465a4f42cb46c28fd32a0d2836c97fc3398179de
Souheil CHELFOUH <trollfot@gmail.com>
2021-12-21 15:58:22 +0100

    Added theme

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
+import typing as t
+import fanstatic
 from functools import partial
 from dataclasses import dataclass, field
 from reiter.application.app import Application
-from reiter.application.request import Request
+from reiter.application.request import Request as BaseRequest
 from reiter.ui import TemplateLoader
 from reiter.ui.components import Page
 from reiter.ui.registry import UIRegistry
 from reiter.view.utils import routables
+from reha.siguv_theme import get_theme
 from roughrider.routing.components import NamedRoutes
 
 
 TEMPLATES = TemplateLoader("./templates")
 
 
+class Request(BaseRequest):
+    user = None
+
+
 @dataclass
 class Reiter(Application):
 
-    ui: UIRegistry = field(default_factory=UIRegistry)
+    request_factory: Request = field(default=Request)
+    ui: UIRegistry = field(
+        default_factory=partial(get_theme, request_type=Request))
     routes: NamedRoutes = field(
         default_factory=partial(NamedRoutes, extractor=routables))
+
+    def get_actions(self, *args, **kwargs):
+        return []
 
     def __call__(self, environ, start_response):
         path = environ['PATH_INFO'].encode('latin-1').decode('utf-8')
@@ -25,24 +37,16 @@ class Reiter(Application):
         return response(environ, start_response)
 
 
-app = Reiter()
+reiter = Reiter()
 
 
-@app.ui.register_layout(Request)
-class Layout:
-
-    _template = TEMPLATES["layout.pt"]
-
-    def __init__(self, request, name):
-        self.name = name
-
-    def render(self, content, **namespace):
-        return self._template.render(content=content, **namespace)
-
-
-@app.routes.register('/')
+@reiter.routes.register('/')
 class Index(Page):
     template = TEMPLATES['index']
 
     def GET(self):
         return {}
+
+
+
+app = fanstatic.Fanstatic(reiter)
